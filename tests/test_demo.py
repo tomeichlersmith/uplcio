6db75a0a47c742957518cd3a419bc680e518231f
tomeichlersmith <eichl008@umn.edu>
2023-11-08 14:05:07 -0600

    expand upon test using ak methods to get assert bools

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,6 +1,14 @@
 import uplcio
+import awkward as ak
 
 def test():
-    array = uplcio.create_demo_array()
+    expected = ak.Array({
+        'one': [1.1, 2.2, 3.3],
+        'two': [[1],[1,2],[1,2,3]]
+    })
+    received = uplcio.create_demo_array()
 
-    assert len(array)==3
+    assert expected.fields == received.fields
+    assert ak.all(expected['one'] == received['one'])
+    assert ak.all(expected['two'] == received['two'])
+
